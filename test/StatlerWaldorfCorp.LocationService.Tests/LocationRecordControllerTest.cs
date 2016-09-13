d69c595f9a69c0aebb357c656f47d4e114989903
Chris Umbel <chris@chrisumbel.com>
2016-09-12 18:20:07 -0600

    ShouldTrackAllLocationsForMember test

--- test/StatlerWaldorfCorp.LocationService.Tests/LocationRecordControllerTest.cs
+++ test/StatlerWaldorfCorp.LocationService.Tests/LocationRecordControllerTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using StatlerWaldorfCorp.LocationService.Models;
 using StatlerWaldorfCorp.LocationService.Controllers;
 using StatlerWaldorfCorp.LocationService.Persistence;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
 
@@ -21,6 +22,26 @@ namespace StatlerWaldorfCorp.LocationService
             controller.AddLocation(memberGuid, new LocationRecord(){ MemberID = memberGuid });
 
             Assert.Equal(1, repository.AllForMember(memberGuid).Count());
+        }
+
+        [Fact]
+        public void ShouldTrackAllLocationsForMember()
+        {
+            ILocationRecordRepository repository = new InMemoryLocationRecordRepository();
+            LocationRecordController controller = new LocationRecordController(repository);
+            Guid memberGuid = Guid.NewGuid();
+
+            controller.AddLocation(memberGuid, new LocationRecord(){ ID = Guid.NewGuid(), Timestamp = 1,
+                MemberID = memberGuid, Latitude = 12.3f });
+            controller.AddLocation(memberGuid, new LocationRecord(){ ID = Guid.NewGuid(), Timestamp = 2,
+                 MemberID = memberGuid, Latitude = 23.4f });
+            controller.AddLocation(Guid.NewGuid(), new LocationRecord(){ ID = Guid.NewGuid(), Timestamp = 3,
+                 MemberID = Guid.NewGuid(), Latitude = 23.4f });                 
+
+            ICollection<LocationRecord> locationRecords = 
+                ((controller.GetLocationsForMember(memberGuid) as ObjectResult).Value as ICollection<LocationRecord>);
+
+            Assert.Equal(2, locationRecords.Count());
         }
     }
 }
