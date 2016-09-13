b2104474ecc555968afd05f4f4aaccf7f4b8fe85
Chris Umbel <chris@chrisumbel.com>
2016-09-12 18:09:23 -0600

    ShouldAdd test

--- test/StatlerWaldorfCorp.LocationService.Tests/LocationRecordControllerTest.cs
+++ test/StatlerWaldorfCorp.LocationService.Tests/LocationRecordControllerTest.cs
@@ -1,6 +1,8 @@
 using Xunit;
 using System.Collections.Generic;
 using StatlerWaldorfCorp.LocationService.Models;
+using StatlerWaldorfCorp.LocationService.Controllers;
+using StatlerWaldorfCorp.LocationService.Persistence;
 using System;
 using System.Linq;
 
@@ -10,9 +12,15 @@ namespace StatlerWaldorfCorp.LocationService
     public class LocationRecordControllerTest
     {
         [Fact]
-        public void Placeholder() 
+        public void ShouldAdd() 
         {
-            Assert.True(true);
+            ILocationRecordRepository repository = new InMemoryLocationRecordRepository();
+            LocationRecordController controller = new LocationRecordController(repository);
+            Guid memberGuid = Guid.NewGuid();
+
+            controller.AddLocation(memberGuid, new LocationRecord(){ MemberID = memberGuid });
+
+            Assert.Equal(1, repository.AllForMember(memberGuid).Count());
         }
     }
 }
