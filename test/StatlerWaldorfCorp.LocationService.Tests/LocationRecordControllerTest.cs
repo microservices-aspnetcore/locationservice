457f89a2b9c5271dee5bb9a68f943e5dec6e78f6
Chris Umbel <chris@chrisumbel.com>
2016-09-12 18:57:05 -0600

    ShouldTrackLatestLocationsForMember

--- test/StatlerWaldorfCorp.LocationService.Tests/LocationRecordControllerTest.cs
+++ test/StatlerWaldorfCorp.LocationService.Tests/LocationRecordControllerTest.cs
@@ -54,10 +54,10 @@ namespace StatlerWaldorfCorp.LocationService
             Guid latestId = Guid.NewGuid();
             controller.AddLocation(memberGuid, new LocationRecord(){ ID = Guid.NewGuid(), Timestamp = 1,
                 MemberID = memberGuid, Latitude = 12.3f });
-            controller.AddLocation(memberGuid, new LocationRecord(){ ID = Guid.NewGuid(), Timestamp = 2,
-                 MemberID = memberGuid, Latitude = 23.4f });
             controller.AddLocation(memberGuid, new LocationRecord(){ ID = latestId, Timestamp = 3,
                  MemberID = memberGuid, Latitude = 23.4f });                 
+            controller.AddLocation(memberGuid, new LocationRecord(){ ID = Guid.NewGuid(), Timestamp = 2,
+                 MemberID = memberGuid, Latitude = 23.4f });
             controller.AddLocation(Guid.NewGuid(), new LocationRecord(){ ID = Guid.NewGuid(), Timestamp = 4,
                  MemberID = Guid.NewGuid(), Latitude = 23.4f });                 
 
