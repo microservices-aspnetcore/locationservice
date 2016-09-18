80a4149fcde8caf17888fd54fa76081b7ac813e2
Chris Umbel <chris@chrisumbel.com>
2016-09-18 11:56:22 -0400

    dll name changed

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /pipeline/source/app/publish
 ls
-dotnet ef database update
-dotnet source.dll --server.urls=http://0.0.0.0:${PORT-"8080"}
+# dotnet ef database update
+dotnet StatlerWaldorfCorp.LocationService.dll --server.urls=http://0.0.0.0:${PORT-"8080"}
