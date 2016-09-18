b5b435e1f8a85efe5e0ce4c187e434a109047022
Chris Umbel <chris@chrisumbel.com>
2016-09-18 13:45:17 -0400

    fixed test project.json

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mkdir -p /root/.nuget/packages/.tools
+mv /pipeline/source/app/ef-tools ~/.nuget/packages/.tools/Microsoft.EntityFrameworkCore.Tools 
 cd /pipeline/source/app/publish
 dotnet ef database update
 dotnet StatlerWaldorfCorp.LocationService.dll --server.urls=http://0.0.0.0:${PORT-"8080"}
