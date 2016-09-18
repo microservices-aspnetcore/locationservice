146e3159f24742e6022753a8bc289d9f418435dc
Chris Umbel <chris@chrisumbel.com>
2016-09-18 13:09:42 -0400

    tools

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p /root/.nuget/packages/.tools
 mv /pipeline/source/app/ef-tools ~/.nuget/packages/.tools/Microsoft.EntityFrameworkCore.Tools 
 cd /pipeline/source/app/publish
 dotnet ef database update
