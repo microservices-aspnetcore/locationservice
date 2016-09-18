482d92014898c9b1b038b3dd796f0f7c5139c806
Chris Umbel <chris@chrisumbel.com>
2016-09-18 12:44:57 -0400

    tools

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mv /pipeline/source/app/tools ~/.nuget/packages/.tools/Microsoft.EntityFrameworkCore.Tools 
+mv /pipeline/source/app/ef-tools ~/.nuget/packages/.tools/Microsoft.EntityFrameworkCore.Tools 
 cd /pipeline/source/app/publish
 dotnet ef database update
 dotnet StatlerWaldorfCorp.LocationService.dll --server.urls=http://0.0.0.0:${PORT-"8080"}
