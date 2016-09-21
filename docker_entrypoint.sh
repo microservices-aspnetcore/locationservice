cd5e105a46b071270c4c97721dc2c30cfa308215
Chris Umbel <chris@chrisumbel.com>
2016-09-20 18:52:48 -0600

    TMPDIR

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -3,4 +3,5 @@ cd /pipeline/source/app/publish
 echo "starting"
 mkdir /pipeline/source/app/publish/tmp
 export TEMP=/pipeline/source/app/publish/tmp
+export TMPDIR=/pipeline/source/app/publish/tmp
 dotnet StatlerWaldorfCorp.LocationService.dll --server.urls=http://0.0.0.0:${PORT-"8080"}
