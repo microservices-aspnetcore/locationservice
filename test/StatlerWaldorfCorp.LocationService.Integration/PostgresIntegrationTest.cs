204e6bdd2189bba75aad546f2feffebc1975a5ae
Chris Umbel <chris@chrisumbel.com>
2016-10-02 21:35:36 -0400

    integration test project created

---
+++ test/StatlerWaldorfCorp.LocationService.Integration/PostgresIntegrationTest.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using System.Collections.Generic;
+using StatlerWaldorfCorp.LocationService.Models;
+using StatlerWaldorfCorp.LocationService.Controllers;
+using StatlerWaldorfCorp.LocationService.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace StatlerWaldorfCorp.LocationService.Integration 
+{
+
+    public class PostgresIntegrationTest
+    {
+        [Fact]
+        public void Postgres() 
+        {
+        }
+    }
+}
